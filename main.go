8faec0734ce9ba43e664c7c7426bef1a76d9e1a7
Donal Byrne <donalb@pagero.com>
2015-11-17 14:04:26 +0100

    Small change to mongo init.

--- main.go
+++ main.go
@@ -5,7 +5,17 @@ import (
 	"github.com/byrnedo/apibase"
 	"net/http"
 	"fmt"
+	"github.com/byrnedo/apibase/db/mongo"
+	"os"
 )
+
+func GetEnvOr(key string, fallback string) string {
+	if val, found := os.LookupEnv; found {
+		return val
+	}
+	return fallback
+}
+
 
 func main() {
 
@@ -16,10 +26,12 @@ func main() {
 
 	apibase.Init()
 
+	mongo.Init(apibase.Conf.GetDefaultString("mongo.url", GetEnvOr("MONGO_URL", "")), Trace)
+
 	http.HandleFunc("/api/v1/healthcheck", healthCheck)
 
 	host = apibase.Conf.GetDefaultString("http.host", "localhost")
-	port = apibase.Conf.GetDefaultInt("http.port", 9999)
+	port = apibase.Conf.GetDefaultInt("http.port", GetEnvOr("PORT", 9999))
 
 	var listenAddr = fmt.Sprintf("%s:%d", host, port)
 	Info.Printf("listening on " + listenAddr)
