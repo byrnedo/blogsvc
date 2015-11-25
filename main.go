abc526dadbd5da7a58c2f54e310228a1e5f9e376
Donal Byrne <byrnedo@tcd.ie>
2015-11-25 22:56:06 +0100

    Adding some structure

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 	"fmt"
 	"github.com/byrnedo/apibase/db/mongo"
 	"github.com/byrnedo/apibase/env"
-	"github.com/byrnedo/apibase/natsio"
-	"github.com/apcera/nats"
+	"github.com/byrnedo/blogsvc/routers"
 )
 
 func main() {
@@ -21,9 +20,9 @@ func main() {
 
 	apibase.Init()
 
-	setupMongo()
+	mongo.Init(env.GetOr("MONGO_URL",apibase.Conf.GetDefaultString("mongo.url", "")), Trace)
 
-	setupNats()
+	routers.InitMq(env.GetOr("NATS_URL", apibase.Conf.GetDefaultString("nats.url", "nats://localhost:4222")))
 
 	http.HandleFunc("/api/v1/healthcheck", healthCheck)
 
@@ -35,21 +34,6 @@ func main() {
 	var listenAddr = fmt.Sprintf("%s:%d", host, port)
 	Info.Printf("listening on " + listenAddr)
 	http.ListenAndServe(listenAddr, nil)
-}
-
-func setupMongo(){
-	mongo.Init(env.GetOr("MONGO_URL",apibase.Conf.GetDefaultString("mongo.url", "")), Trace)
-}
-
-func setupNats(){
-	var natsOpts *natsio.Nats
-	natsOpts = natsio.NewNats(func(n *natsio.Nats) error{
-		n.Url = env.GetOr("NATS_URL", apibase.Conf.GetDefaultString("nats.url", "nats://localhost:4222"))
-		return nil
-	})
-	natsOpts.HandleFunc("blog.healthcheck", func(m *nats.Msg){
-
-	})
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
